8c5e3a15ede0424f1236ba623f1eef1241f8bcf0
fuadop <fuadolatunji@gmail.com>
2020-11-16 07:36:46 +0100

    update: learned about interfaces but cannot seem to understand writer and reader interfaces

--- http/main.go
+++ http/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
+type logWriter struct{}
+
+func main() {
+	response, err := http.Get("http://google.com")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	lw := logWriter{}
+
+	io.Copy(lw, response.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes: ", len(bs))
+	return len(bs), nil
 }
